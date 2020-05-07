23541e8eca51948dc8136e53248c4fff5af9b50f
Louis Kerner <louiskerner@gmx.de>
2020-05-07 16:34:55 +0200

    Form und Config-Name: Klausurenchecker

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,19 @@
 @section('content')
 <div class="title m-b-md">
             <div class="content">
-                    Laravel
+                    {{config('app.name')}}
                 </div>
-
+                <form action="{{ url('/submit')}}" method="POST" class="form-horizontal">
+                @csrf <!-- wird benötigt für POST siehe Laravel-Doc   -->
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                  </div>
+                  <input type="text" class="form-control" id="eDate" name="eDate" maxlength="30" placeholder="Tag prüfen"
+                  required autofocus><br>
+                </div>
+                <input type="submit" id="submit" class="btn btn-primary float-right">
+            </form>
                 </div>
             </div>
         </div>
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -9,13 +9,16 @@
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
                 <!-- Latest compiled JavaScript -->
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+                <!-- Glyphicons: Font-Awesome -->
+                <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
     </head>
-    
+
     <body>
       <!-- Navigation -->
       <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="/">
+          <a class="navbar-brand" href="/"></a>
             <!-- <img class="pr-1 pd-1"src="img/logo.svg" alt="Logo" style="width:30px;">{{config('app.name')}}</a> -->
           @yield('nav')
         </div>
