ace905ef80a0030eddd5276a6696e33b70b1df67
Louis Kerner <louiskerner@gmx.de>
2020-05-07 16:50:42 +0200

    submit and receive value

--- app/Http/Controllers/KlausurController.php
+++ app/Http/Controllers/KlausurController.php
@@ -9,4 +9,7 @@ class KlausurController extends Controller
     public function index(){
       return view('index');
     }
+    public function submit(Request $request){
+      return view('index')->with([ 'possible'=> $request['eDate']]);
+    } 
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,17 +9,20 @@
             <div class="content">
                     {{config('app.name')}}
                 </div>
-                <form action="{{ url('/submit')}}" method="POST" class="form-horizontal">
+                <form action="{{ url('/submit')}}" method="GET" class="form-horizontal">
                 @csrf <!-- wird benötigt für POST siehe Laravel-Doc   -->
                 <div class="input-group">
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                   </div>
-                  <input type="text" class="form-control" id="eDate" name="eDate" maxlength="30" placeholder="Tag prüfen"
+                  <input type="date" value="yyyy-mm-dd" class="form-control" id="eDate" name="eDate" maxlength="30" placeholder="yyyy-mm-dd"
                   required autofocus><br>
                 </div>
                 <input type="submit" id="submit" class="btn btn-primary float-right">
             </form>
+            @if(!empty($possible))
+              {{$possible}}
+            @endif
                 </div>
             </div>
         </div>
